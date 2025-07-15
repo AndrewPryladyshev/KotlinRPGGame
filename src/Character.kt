af8755236cd772486b2956c0fb3a052657a059bd
AndrewKyslyi <skorpypro@gmail.com>
2025-07-15 20:38:21 +0300

    start

--- src/Character.kt
+++ src/Character.kt
@@ -7,6 +7,23 @@ class Character(
     val magDamage: Int,
     var type: String?,
     var attackSpeed: Float,
+    var spells: MutableList,
+    var weapon: 
 ) {
+  fun physical_attack(): Int {
+    // smthing that will be returned
+    var physical_damage
+  }
 
+  fun block() {
+
+  }
+
+  fun death() {
+    
+  }
+
+  fun magical_attack() : Int {
+
+  }
 }
